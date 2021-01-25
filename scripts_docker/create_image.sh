054f1015089860416edf2759b631d13b5c4477a4
UnDesSix <facebook.matthieu@yahoo.fr>
2021-01-25 23:13:38 +0100

    Removing of comments

--- scripts_docker/create_image.sh
+++ scripts_docker/create_image.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
 docker build -t ft_server ../.
-
-# docker run -d -p 80:80 -p 443:443 ft_server
-
-# docker exec -it $(docker ps -q | head -1) bash
-
-# docker exec -it $(docker run -d -p 80:80 -p 443:443 ft_server) bash
